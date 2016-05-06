fe16c14362309fa44f8428c4f97021228e174a89
Yunus AK <akyunus@users.noreply.github.com>
2016-05-06 10:53:20 +0300

    Update PdoPager.php

--- PdoPager.php
+++ PdoPager.php
@@ -1,27 +1,28 @@
-<?php namespace Bny\Helpers;
+<?php namespace AGTR;
 /**
  * 
- * SQL sorgu sonuçlarına LIMIT ekleyerek otomatik sayfalama yapar. 
- * sayfalama meta verisini otomatik oluşturur.
- * Pdo ile çalışır.
- * sql sorguları SELECT SQL_CALC_FOUND_ROWS ile yapılmalı
+ * SQL sorgu sonuÃ§larÃ½na LIMIT ekleyerek otomatik sayfalama yapar. 
+ * sayfalama meta verisini otomatik oluÃ¾turur.
+ * Pdo ile Ã§alÃ½Ã¾Ã½r.
+ * sql sorgularÃ½ SELECT SQL_CALC_FOUND_ROWS ile yapÃ½lmalÃ½
  * 
  * @author Yunus AK <yunus@ak.gen.tr>
  */
 class PdoPager 
 {
 
-  // sayfa başına kayıt sayısı.		
+  // records per page		
   private $_pp = 15;
-  //toplam kayıt sayısı
+  // total record cout
   private $_total = 0;
-  // mevcut sayfa no
+  // current page record count, 
+  private $_count = 0;  
+  // current page no
   private $_current = 0;
-  // son sayfa no yani toplam sayfa adedi
+  // total page no
   private $_last = 0;
-  // mevcut sayfadaki kayıt sayısı, son sayfa değilse _pp'ye eşit
-  private $_count = 0;
-  
+
+  // this class 
   private $_pdo;
   
   
@@ -31,61 +32,58 @@ class PdoPager
   	$this->_pp	= (isset($opts['per_page'])?$opts['per_page'] : $this->_pp);
   }
   
-  // sql sorgusu SELECT SQL_CALC_FOUND_ROWS ifadesi ile başlıyorsa 
-  // getPaginationMeta fonksiyonu düzgün çalışacaktır. performans için bu yöntem kullanıldı.
-  // Bu fonksiyon verilen page no ile belirtilen sayfa numarasında bulunan kayıtların listesini
-  // array formatında döndürür.
   public function paginate($sql,$vars = null,$page = 1,$perpage = null, $getmeta = false)
   {
 	
   	$perpage 	= (is_null($perpage)) 	? $this->_pp 	: $perpage;
-  	$page    	= ($page < 1) 			? 1 			: $page;
-  	
-		$start 		= $perpage * ($page - 1);
+  	$page    	= ($page < 1)		? 1 		: $page;
+	$start 		= $perpage * ($page - 1);
+	
+	$this->_current = $page;
+	
+	$sql .= " LIMIT $start, $perpage";
 		
-		$sql .= " LIMIT $start, $perpage";
-		
-	// tüm sorguları error_log dosyasına kaydetmek için...
-	// error_log("Pager SQL : $sql");
 	try
 	{
-
 		$stmt = $this->_pdo->prepare($sql);
 		$stmt->execute($vars);
-    }
+	}
 	catch(\PDOException $e)
 	{
 		error_log($e->getMessage());
 	}
 	
-    $data = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	$data['results'] = $stmt->fetchAll(\PDO::FETCH_ASSOC);
     
-    // toplam kayıt sayısını bul
-    $this->_total = $this->_pdo->query("SELECT FOUND_ROWS()")->fetchColumn();
-    
-    $this->_current = $page;
-    
-    $this->_last = floor((($this->_total - 1) / $perpage) + 1);
-    
-    //TODO: mevcut kayıt sayısını hesapla
-    if ($this->_current == $this->_last) 
-    {
-    	$this->_count   = count($data);
-    } else {
-    	$this->_count   = $perpage;	
-    }
-    
-    
+	
+	
+	$data['pagination'] = $this->getPaginationMeta();
     
     return $data;
-    
-  }
+}
   
-  // paginate ile sonuçlar alındıktan sonra getpaginationmeta ile sayfalama bilgisi alınabilir.
-  // sql sorgusu SELECT SQL_CALC_FOUND_ROWS ifadesi ile başlıyorsa 
-  // getPaginationMeta fonksiyonu anlamlı çalışacaktır.
-  public function getPaginationMeta()
+  // paginate ile sonuÃ§lar alÃ½ndÃ½ktan sonra getpaginationmeta ile sayfalama bilgisi alÃ½nabilir.
+  // sql sorgusu SELECT SQL_CALC_FOUND_ROWS ifadesi ile baÃ¾lÃ½yorsa 
+  // getPaginationMeta fonksiyonu anlamlÃ½ Ã§alÃ½Ã¾acaktÃ½r.
+  private function getPaginationMeta()
   {
+  	// get total found rows
+	$this->_total = $this->_pdo->query("SELECT FOUND_ROWS()")->fetchColumn();
+	
+	// current page no is known
+	
+	
+	// calcute total number of pages
+	$this->_last = floor((($this->_total - 1) / $perpage) + 1);
+	
+	// only if in last page, current pages record count may be 
+	if ($this->_current == $this->_last) 
+	{
+	    $this->_count   = count($data);
+	} else {
+	    $this->_count   = $perpage;	
+	}
+  	
   	$meta = array(
   			"total" => $this->_total,
   			"count" => $this->_count,
